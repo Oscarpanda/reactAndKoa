91e18dca29d92e84095d8da4d9fb6e07cea3ac9c
gaoxiong.wu <wugaoxiong@100tal.com>
2020-01-12 16:43:18 +0800

    feat: templating

--- server/app.js
+++ server/app.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Koa from 'koa';
+import routes from "./router";
+import templating from "./templating"
 import {
   renderToString
 } from "react-dom/server";
 
 const app = new Koa();
 
-const App = () => < div > Hello Koa SSR < /div>
-
-app.use(ctx => {
-  ctx.body = renderToString( < App / > );
-})
+app.use(templating);
+app.use(
+  routes.routes(), routes.allowedMethods()
+);
 
 app.listen(9000, () => {
   console.log(`node服务已经启动, 请访问localhost:9000`)
--- server/router/index.js
+++ server/router/index.js
@@ -1,17 +1,23 @@
-const api = require("./api/index");
-const koaRouter = require("koa-router");
-const home = new koaRouter()
-home.get("/", async (ctx) =>{
-  let html = `
-    <ul>
-      <li><a href="/page/helloworld">/page/helloworld</a></li>
-      <li><a href="/page/404">/page/404</a></li>
-    </ul>
-  `
-  ctx.body = html
+import Router from 'koa-router';
+import RouterConfig from '../../app/router';
+import {
+  StaticRouter
+} from 'react-router-dom';
+import {
+  renderToString
+} from "react-dom/server";
+import React from 'react';
 
+const routes = new Router();
+
+routes.get('/', (ctx, next) => {
+  ctx.render()
+  next();
 })
-const routers = new koaRouter(); 
-routers.use("/api", api.routes())
-routers.use("/", home.routes())
-module.exports = routers;
+
+routes.get('/list', (ctx, next) => {
+  ctx.render()
+  next();
+})
+
+export default routes;
