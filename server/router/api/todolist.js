9f22abf1ad77635952d60872ac8a4f5362666129
gaoxiong.wu <wugaoxiong@100tal.com>
2020-02-10 22:51:25 +0800

    fix: 默认显示active

--- server/router/api/todolist.js
+++ server/router/api/todolist.js
@@ -3,7 +3,7 @@ const  todoListModel = require("./../../db/todoList");
 const todoList = new Router();
 todoList.post("/findListByName", async (ctx) => {
   let name = ctx.request.body.name;
-  let result = await todoListModel.find({name}).sort("updated": -1);
+  let result = await todoListModel.find({name}).sort({"updated": -1});
   console.log("result");
   ctx.body = result;
 })
